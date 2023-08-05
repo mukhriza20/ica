68ec6b21e6d5e22631f99b4dcd41d224e6fa9c6a
mukhriza tunnisa <ica@192.168.1.3>
2023-08-05 16:18:46 +0700

    first commit

---
+++ Desktop/hw-9/hw_9/controller/user.controller.js
@@ -0,0 +1,103 @@
+/**
+* @swagger
+* components:
+*   schemas:
+*     Movies:
+*       type: object
+*       required:
+*         - email
+*         - genre
+*         - password
+*       properties:
+*         id:
+*           type: string
+*           description: The auto-generated id of the film
+*         title:
+*           type: string
+*           description: The movies title
+*         genre:
+*           type: string
+*           description: The genres of the movies
+*         year:
+*           type: string
+*           description: The year of the movies released 
+*         createdAt:
+*           type: string
+*           format: date
+*           description: The date the movie was added
+*       example:
+*         id: d5fE_asz
+*         title: fast and 
+*         genre: action
+*         year: "2019"
+*         createdAt: 2020-03-10T04:05:06.157Z
+*/
+
+
+
+const express = require('express')
+const router = express.Router()
+const db = require('./../models')
+const User = db.User
+const authenticateTokenMiddleware = require('../middleware/authentication')
+const swaggerJSDoc = require('swagger-jsdoc')
+
+// Inject middleware as global middleware
+router.use(authenticateTokenMiddleware)
+
+// GET /users
+router.get('/api/users', async (request, response) => {
+  const users = await User.findAll({ offset: request.query.page, limit: request.query.size })
+  const userCount = await User.count()
+
+  return response.status(200).json({
+    data: users,
+    meta: {
+      page: request.query.page,
+      count: userCount,
+      size: users.length,
+    }
+  })
+})
+
+// GET /users/:id
+router.get('/api/users/:id', async (request, response) => {
+  const user = await User.findByPk(request.params.id)
+
+  if(!user) return response.status(404).json({message: 'Users not found'})
+
+  return response.status(200).json({data: user})
+})
+
+// POST /users
+router.post('/api/users', async (request, response) => {
+  const user = await User.create(request.body)
+
+  if(!user) return response.status(422).json({message: 'Failed create user. Please try again'})
+
+  return response.status(200).json({data: user})
+})
+
+// PUT /users/:id
+router.put('/api/users/:id', async (request, response) => {
+  const user = await User.findByPk(request.params.id)
+
+  if(!user) return response.status(404).json({message: 'Users not found'})
+
+  User.update(request.body, { where: {id: request.params.id }})
+
+  return response.status(200).json({ message: 'User updated'})
+})
+
+// DELETE /users/:id
+router.delete('/api/users/:id', async (request, response) => {
+  const user = await User.findByPk(request.params.id)
+
+  if(!user) return response.status(404).json({message: 'Users not found'})
+
+  User.destroy({where: { id: request.params.id }})
+
+  return response.status(200).json({data: user})
+})
+
+module.exports = router
